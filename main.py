6300e7872140234abf828969117d3d70fb05628f
Luke Moran <luke.moran2004@gmail.com>
2021-07-03 10:49:54 +0100

    Completed objective 2. Error is reported if mode inputted is incorrect.

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
 import sys
 from UI import GUI, Terminal
 
+def usage():
+    print("run with t as second argument for terminal mode")
+    print("run with g as second argument for GUI mode")
+    print("-"*40+"\n")
 
 
 
 
 if __name__=="__main__":
-    print("run with t as second argument for terminal mode")
-    print("run with g as second argument for GUI mode")
-    print("-"*40+"\n")
-    sys_args = sys.argv
+    usage()
 
     isTerminal: bool = True
+    isBad: bool = False
 
-
-    if len(sys_args)==1:
+    if len(sys.argv)==1:
         print("Assumed running from Pycharm, would you like terminal or GUI?")
-        isTerminal = input("Enter mode? (t|g): ") == "t"
-    else:
-        if sys_args[1]=="t":
+        inMode = input("Enter mode? (t|g): ")
+        if inMode not in "tg":
+            isBad = True
+        elif inMode=="t":
             isTerminal = True
-        elif sys_args[1]=="g":
+        elif inMode=="g":
             isTerminal = False
 
-    if isTerminal:
-        ui = Terminal()
     else:
-        ui = GUI()
+        if sys.argv[1]=="t":
+            isTerminal = True
+        elif sys.argv[1]=="g":
+            isTerminal = False
+        else:
+            isBad = True
 
-    ui.run()
+    if not isBad:
+        if isTerminal:
+            ui = Terminal()
+        else:
+            ui = GUI()
+
+        ui.run()
+    else:
+        print("Bad arguments, please try again")
