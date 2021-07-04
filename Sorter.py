d456e478ce8edb00e2793537443eeb59dd44c906
Luke Moran <luke.moran2004@gmail.com>
2021-07-04 14:38:47 +0100

    Completed objective 1.4a) - Added functional merge sort method. Also added functionality for Bubble sort and quick sort methods to be implemented within the Sorter class.

--- Sorter.py
+++ Sorter.py
@@ -6,30 +6,79 @@ class Sorter:
     SMALL_SORT = 8
     MED_SORT = 15
     LARGE_SORT = 25
+    SORT_SIZES = [SMALL_SORT,MED_SORT,LARGE_SORT]
+    NUMBER_SORTS = 3
+    NUM_SIZES = 3
+
+
     def __init__(self) -> None:
-        pass
+
+        self.MENU_LOOKUP = ["", self.display_type_menu, self.display_size_menu]
 
 
+    def getValidChoice(self, upper: int, menuChoice: int = 0, lower: int = 1) -> int:
 
 
-    def generate_array(self, upto:int ) -> List[int]:
+        while True:
+            self.MENU_LOOKUP[menuChoice]()
+            str_choice: str = input(f"Enter choice({lower}-{upper}): ")
+
+            try:
+                int_choice: int = int(str_choice)
+
+                if lower <= int_choice <= upper:
+                    return int_choice
+
+                print("Please input a number which is in range")
+            except:
+                print("Invalid input, please enter an integer")
+
+
+    def generate_array(self, upto: int ) -> List[int]:
         nums = [i+1 for i in range(upto)]
 
         random.shuffle(nums)
 
         return nums
 
+    def display_type_menu(self):
+        print("-"*20, "Sorting choice menu","-"*20)
+        print("1) For Merge sort")
+        print("2) For Bubble sort")
+        print("3) For Quick sort")
+
+        #print(f"Enter choice (1-{Sorter.NUMBER_SORTS})")
+
+    def display_size_menu(self):
+        print("-"*20, "Size menu", "-"*20)
+        print("1) For Small array (8 elements)")
+        print("2) For Medium array (15 elements)")
+        print("3) For Large array (25 elements")
+
     def process_sorting_choice(self) -> None:
 
-        to_sort = self.generate_array(Sorter.MED_SORT)
+        typeSort = self.getValidChoice(Sorter.NUMBER_SORTS, 1) # With menu 1 - Types menu
 
-        # First merge sort function will be out of place
-        # Must define the array as an attribute that can be accessed from the class and modified
+        # 1 for merge, 2 -> bubble, 3 -> quick
+
+        chosenSize = self.getValidChoice(3, 2) # Upper limit of 3 with menu 2 - Size menu
+
+        # 1 -> small, 2 -> medium, 3 -> large
+
+        to_sort: List[int] = self.generate_array(Sorter.SORT_SIZES[chosenSize-1])
 
 
-        # This way the array can be printed inside the merge sort function, which makes the process much clearer
+        if typeSort==1:
+            # Merge sort
+            self.mergeSort(to_sort)
+        elif typeSort==2:
+            # Bubble sort
+            self.bubbleSort(to_sort)
+        elif typeSort==3:
+            # Quick sort
+            self.quickSort(to_sort)
 
-        self.mergeSort(to_sort)
+
     def mergeSort(self, arr:List[int], level:int=1) -> List[int]:
 
         mid = len(arr)//2
@@ -73,3 +122,10 @@ class Sorter:
 
         print(f"Merged array at level {level}", merged)
         return merged
+
+
+    def bubbleSort(self, arr:List[int] ) -> None:
+        raise NotImplementedError
+
+    def quickSort(self, arr:List[int] ) -> None:
+        raise NotImplementedError
