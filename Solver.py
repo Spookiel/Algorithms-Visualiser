b1614d05c9534e6e15ec8a4123b3f27e8579b3e5
Luke Moran <luke.moran2004@gmail.com>
2021-07-10 12:09:14 +0100

    Added typing for returns and arguments in GridSolver class.

--- Solver.py
+++ Solver.py
@@ -26,7 +26,7 @@ class GridSolver():
         self.end: Tuple[int, int] = 0, 0
         self.parents = {}
 
-    def locateStart(self) -> Tuple[int, int]:
+    def locate_start(self) -> Tuple[int, int]:
         # "s" in colored("s", "blue") -> True
         # "s" == colored("s", "blue") -> False
         for y in range(len(self._curGrid)):
@@ -36,7 +36,7 @@ class GridSolver():
         print("START POINT NOT FOUND, USING TOP LEFT CORNER")
         return 0,0
 
-    def locateEnd(self) -> Tuple[int, int]:
+    def locate_end(self) -> Tuple[int, int]:
         # "e" in colored("e", "blue") -> True
         # "e" == colored("e", "blue") -> False
         for y in range(len(self._curGrid)):
@@ -46,44 +46,45 @@ class GridSolver():
         print("END POINT NOT DEFINED, USING BOTTOM RIGHT CORNER")
         return len(self._curGrid)-1, len(self._curGrid)-1
 
-    def checkLim(self, node):
+    def checkLim(self, node: Tuple[int, int]) -> bool:
         # Checks if a node is on the grid or not
         return 0 <= node[0] < len(self._curGrid) and 0 <= node[1] < len(self._curGrid)
 
-    def getVal(self, node):
+    def getVal(self, node: Tuple[int, int]):
         if self.checkLim(node):
             return self._curGrid[node[1]][node[0]]
     def bfs(self):
         # Generates a sequence of colored grids,
         # yellow is for items currently in the queue, green is for items that have already been seen
         # Function works with manhattan distance
-        self.parents = {}  # Stores parent node for each node so a path to start can be re-traced
-        seen = set()
-        lastDist = 0
-        self.start = self.locateStart()
-        self.end = self.locateEnd()
+        self.parents: dict = {}  # Stores parent node for each node so a path to start can be re-traced
+        seen: set = set()
+        last_dist: int = 0
+        self.start: Tuple[int, int] = self.locate_start()
+        self.end: Tuple[int, int] = self.locate_end()
         self._pathSteps = []
         queue = [(self.start, 0)]  # (Node, distance)
         seen.add(self.start)
         while queue:
-            nextNode, dist = queue.pop(0)
-            #print(dist, nextNode, lastDist)
-            valAt = self.getVal(nextNode)
+            next_node, dist = queue.pop(0)
 
-            if lastDist!=dist:
+            val_at = self.getVal(next_node)
+
+            if last_dist != dist:
                 # Record grid state and update last dist
-                lastDist = int(dist) # Careful of copying errors
+                last_dist = int(dist) # Careful of copying errors
 
                 copGrid = []
                 for row in self._curGrid:
                     copGrid.append(row[:])
                 self._steps.append(copGrid)
-            # Colour green because have been searched
-            if GridCreator.STILE not in valAt and GridCreator.ETILE not in valAt:
-                # Not a start or end point so should colour green
-                self._curGrid[nextNode[1]][nextNode[0]] = colored(GridCreator.TILE, "green")
 
-            if GridCreator.ETILE in valAt:
+            # Colour green because have been searched
+            if GridCreator.STILE not in val_at and GridCreator.ETILE not in val_at:
+                # Not a start or end point so should colour green
+                self._curGrid[next_node[1]][next_node[0]] = colored(GridCreator.TILE, "green")
+
+            if GridCreator.ETILE in val_at:
                 # Reached end so stop
                 break
 
@@ -91,19 +92,19 @@ class GridSolver():
             # Go to adjacent nodes
 
             for dx, dy in [[0,1], [1,0], [-1,0], [0,-1]]:
-                nx, ny = nextNode[0]+dx, nextNode[1]+dy
+                nx, ny = next_node[0]+dx, next_node[1]+dy
                 adjNode = (nx, ny)
 
                 if self.checkLim(adjNode) and adjNode not in seen and "#" not in self.getVal(adjNode):
                     # New node is inside grid, and is not a barrier
                     seen.add(adjNode)
-                    self.parents[adjNode] = nextNode
+                    self.parents[adjNode] = next_node
 
 
                     # Colour this node yellow on the grid
                     if GridCreator.ETILE not in self.getVal(adjNode):
                         self._curGrid[adjNode[1]][adjNode[0]] = colored(GridCreator.TILE, "blue")
-                    #print("Adding", adjNode, "From", nextNode, "Dist", dist)
+                    #print("Adding", adjNode, "From", next_node, "Dist", dist)
                     queue.append((adjNode, dist+1))
 
 
@@ -136,7 +137,7 @@ class GridSolver():
 
         for step in self._pathSteps:
             print("-"*40)
-            time.sleep(0.6)
+            time.sleep(0.5)
             for row in step:
                 print(*row)
 
